7bb562e09a31c27b5c98e942d4c62b1d1d3eafcc
Izabella Araujo <izabellaa244@gmail.com>
2026-01-25 00:04:01 -0300

    adiciona item dinamicamente na lista

--- formulario/listaDeTarefas.js
+++ formulario/listaDeTarefas.js
@@ -1,15 +1,17 @@
 const criarTarefa = (evento) => {
     evento.preventDefault() // previne o comportamento padrão do botão (submissão do formulário)
+    
+    const lista = document.querySelector('[data-list]') // seleciona a lista de tarefas
     const input = document.querySelector('[data-form-input]') // seleciona o campo de entrada de texto
     const valor = input.value // obtém o valor digitado no campo de entrada
 
-    console.log( valor ) // exibe o valor no console
-
-    const tarefa = document.querySelector('[data-task]') // seleciona a lista de tarefas
+    const tarefa = document.createElement('li') // cria um novo elemento <li>
+    tarefa.classList.add('task') // atribui a classe 'task' ao novo elemento <li>
     const conteudo = `<p class="content"> ${valor}</p>` // cria uma string vazia para o conteúdo da nova tarefa
 
-    tarefa.innerHTML = conteudo // define o conteúdo da nova tarefa
-
+    tarefa.innerHTML = conteudo // define o conteúdo HTML do novo elemento <li>
+    
+    lista.appendChild(tarefa) // adiciona o novo elemento <li> à lista de tarefas
     input.value = '' // limpa o campo de entrada após adicionar a tarefa
 }
 
